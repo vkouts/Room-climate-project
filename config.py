b08abfd423877abdc4a4ac437c2b30a51ddfe17f
Kouts Vladimir <kvn12@ukr.net>
2018-05-06 15:32:54 +0000

    Use mysql instead

--- config.py
+++ config.py
@@ -3,5 +3,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'climate.db')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'room_climate')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'climate')
+    MYSQL_PASS = os.environ.get('MYSQL_PASS', 'climate')
+    SQLALCHEMY_DATABASE_URI = 'mysql://{user}:{password}@{host}/{db}'.format(
+      user=MYSQL_USER,
+      password=MYSQL_PASS,
+      host=MYSQL_HOST,
+      db=MYSQL_DB
+    )
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'climate.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
